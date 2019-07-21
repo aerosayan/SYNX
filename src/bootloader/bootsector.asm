cb1650c873fc843cb389943d6a3b0dadf2673a37
sayan <aero.sayan@gmail.com>
2019-07-22 01:23:13 +0530

    Used tty mode and interrupt 0x10 to print 'Hello' on screen

--- src/bootloader/bootsector.asm
+++ src/bootloader/bootsector.asm
@@ -1,8 +1,25 @@
-; Infinite jump loop
-loop:
-  jmp loop
+;------------------------------------------------------------------------------
+; Activate tty mode my setting 'ah' to '0x0e' and then move each particular
+; character to 'al' and calling interrupt 0x10 to print them on screen.
+;------------------------------------------------------------------------------
+mov ah, 0x0e             ; Activate tty mode
+mov al, 'H'              ; Character to print
+int 0x10                 ; Print to screen
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
 
-; Write the bootsector
+
+
+jmp $                    ; Infinite jump loop to current address
+
+;------------------------------------------------------------------------------
+; Insert padding and magic number
+;------------------------------------------------------------------------------
 ; Fill with (510-size of previous code) zeroes
 times 510 - ($-$$) db 0
 
